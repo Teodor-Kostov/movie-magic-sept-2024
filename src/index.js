be825803b4cfc1f24e02f12bcad88ffd54c06165
Teodor-Kostov <teodorkostov88@gmail.com>
2024-09-23 00:21:53 +0200

    Setup express-handlebars

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
-import { log } from "console";
 import express from "express";
+import handlebars from "express-handlebars"
 
 const app = express();
 const port = 5000;
 
+app.engine("hbs", handlebars.engine({
+    extname: "hbs",
+}));
+
+app.set('view engine', 'hbs');
+app.set('views', './src/views');
+
 
 app.get('/', (req, res)=>{
-    res.send('<h1>It is working!</h1>');
-})
+    res.render('index', {layout: false})
+});
 
 app.listen(port, ()=>console.log(`Server is listening on http://localhost:${port}...`));
