db019903295057eb813fd94d49d4282ddeeff742
Teodor-Kostov <teodorkostov88@gmail.com>
2024-09-25 23:07:13 +0200

    Add body parser

--- src/index.js
+++ src/index.js
@@ -14,6 +14,7 @@ app.set('view engine', 'hbs');
 app.set('views', './src/views');
 
 app.use(express.static('public'));
+app.use(express.urlencoded({extended: false}));  // middleware to parse the bode from the html form
 
 app.use(routes);
 
