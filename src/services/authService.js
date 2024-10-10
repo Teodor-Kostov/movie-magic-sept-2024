861513ef57af6325dd6ee9ad9103f56220370a37
Teodor-Kostov <teodorkostov88@gmail.com>
2024-10-10 14:39:34 +0200

    Genera jwt Token on successful login

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,5 +1,8 @@
 import User from "../models/Users.js";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+const SECRET = 'ff77f90a2b3cfb287559a1837fd2aH79fe';
 
 const register = async (email, password) =>{
 
@@ -14,17 +17,24 @@ const login = async(email, password)=>{
     const user = await User.findOne({email: email}); // checking if the User exists
 
     if(!user){
-        throw new Error('User does not exist!')
+        throw new Error('User does not exist!');
     };
 
     const isValid = await bcrypt.compare(password, user.password); // compare the pass with hash pass
 
     if(!isValid){
-        throw new Error ('Incorrect password!')
-    }
-
-
+        throw new Error ('Incorrect password!');
+    };
     //ToDo: Return jwt token
+    const payload = {
+        _id: user._id,
+        email,
+    }
+    const token = jwt.sign(payload, SECRET, {expiresIn: '1h'});
+
+    return token;
+    
+    
 };
 
 
