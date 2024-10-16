e02003e83e6aa5d09eebf23f267d1c04bb402265
Teodor-Kostov <teodorkostov88@gmail.com>
2024-10-16 15:53:32 +0200

    Update Movie.js
    Adding custom validation for rating and err msgs for the model props

--- src/models/Movie.js
+++ src/models/Movie.js
@@ -3,26 +3,26 @@ import { Schema, model, Types } from "mongoose";
 const movieSchema = new Schema({
     title: {
         type: String,
-        required: true,
+        required: [true, "Title is required!"],
         minlength: 5,
         validate: [/^[A-Za-z0-9 ]+$/, "Title contains invalid characters" ]  //form beginning to the end must contain A-Za--z0-9 and white space
     },
     genre: {
         type: String,
-        required: true,
+        required: [true, "Genre is required!"],
         lowercase: true,
         validate: [/^[A-Za-z0-9 ]+$/, "Genre exceeds character limit or contains invalid characters" ]
     },
     director: {
         type: String,
-        required: true,
+        required: [true, "Director name is required!"],
         minlength: 5,
         validate: [/^[A-Za-z0-9 ]+$/, "Director contains invalid characters" ]
 
     },
     year: {
         type: Number,
-        required: true,
+        required: [true, "Year is required!"],
         min: [1900, "Can not add movies before 1900!"],
         max: [2050, "Can not add movies after 2050"]
 
@@ -45,7 +45,7 @@ const movieSchema = new Schema({
     },
     description: {
         type: String,
-        required: true,
+        required: [true, "Description is required!"],
         minlength:[20, "Description mus be at least 20 characters long."],   
         validate: [/^[A-Za-z0-9 ]+$/, "Description contains invalid characters." ]
     },
